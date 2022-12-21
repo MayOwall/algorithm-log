e0d38ca45f6b3465af81e6e350d8cf081020b0aa
MayOwall <pixartive@gmail.com>
2022-12-21 17:29:44 +0900

    JS, 22. 옷가게 할인 받기

---
+++ Javascript/Programmers/22.옷가게_할인_받기.js
@@ -0,0 +1,8 @@
+const solution = (price) =>
+  price >= 500000
+    ? Math.floor(price * 0.8)
+    : price >= 300000
+    ? Math.floor(price * 0.9)
+    : price >= 100000
+    ? Math.floor(price * 0.95)
+    : price;
