d59e37936fb8b25888e3d2e83c446f74a5691676
MayOwall <pixartive@gmail.com>
2022-12-22 16:58:45 +0900

    JS, 32. 세균 증식 & 33. n의 배수 고르기

--- Javascript/Programmers/32.세균_증식.js
+++ Javascript/Programmers/32.세균_증식.js
@@ -1,1 +1,8 @@
-const solution = (n, t) => n * Math.pow(2, t);
+// const solution = (n, t) => n * Math.pow(2, t);
+
+const solution = (n) => {
+  const arr = new Array(n)
+    .fill("")
+    .map((_, idx) => new Array(idx + 1).fill("*").join(""));
+  console.log(arr);
+};
