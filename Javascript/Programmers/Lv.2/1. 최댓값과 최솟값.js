8c8e537ccf21f2b6026c24bfddd1ed64ebee2503
Owall <97934878+MayOwall@users.noreply.github.com>
2023-02-24 07:19:34 +0900

    JS, Programmers, Lv.2, 최대값과 최솟값

--- Javascript/Programmers/Lv.2/1. 최댓값과 최솟값.js
+++ Javascript/Programmers/Lv.2/1. 최댓값과 최솟값.js
@@ -2,3 +2,10 @@ const solution = (s) => {
   const arr = s.split(" ").map((v) => Number(v));
   return `${Math.min(...arr)} ${Math.max(...arr)}`;
 };
+
+// 2023.02.24
+const solution2 = (s) => {
+  // sort를 해 주는 과정에서 계산으로 인해 숫자로 자동 형변환이 됨.
+  const arr = s.split(" ").sort((a, b) => a - b);
+  return `${arr[0]} ${arr[arr.length - 1]}`;
+};
