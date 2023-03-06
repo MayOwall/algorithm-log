ca9353446b7bd33c97f66235bb641bc84a4352c5
Owall <97934878+MayOwall@users.noreply.github.com>
2023-03-06 17:49:54 +0900

    JS, Baekjoon, 15552. 빠른 A+B

---
+++ Javascript/Baekjoon/15552. 빠른 A+B.js
@@ -0,0 +1,10 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+let answer = "";
+
+for (let i = 1; i <= input[0]; i++) {
+  let num = input[i].split(" ");
+  answer += Number(num[0]) + Number(num[1]) + "\n";
+}
+
+console.log(answer);
