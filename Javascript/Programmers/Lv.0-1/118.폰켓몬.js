d07a6bc2aa2c441a09acd2ce94e6939da55dba2c
Owall <97934878+MayOwall@users.noreply.github.com>
2023-04-06 18:19:13 +0900

    JS, Programmers, 해시-폰켓몬

--- Javascript/Programmers/Lv.0-1/118.폰켓몬.js
+++ Javascript/Programmers/Lv.0-1/118.폰켓몬.js
@@ -1,2 +1,9 @@
+// 1
 const solution = (nums) =>
   new Set(nums).size < nums.length / 2 ? new Set(nums).size : nums.length / 2;
+
+// 2
+const solution2 = (nums) => {
+  const typeNum = new Set(nums).size;
+  return typeNum < nums.length / 2 ? typeNum : nums.length / 2;
+};
