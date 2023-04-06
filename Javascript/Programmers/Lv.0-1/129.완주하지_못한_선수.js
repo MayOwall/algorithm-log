a997d9f17c34361f31d1c0b1d18afb7eacbce3b8
Owall <97934878+MayOwall@users.noreply.github.com>
2023-04-06 18:28:11 +0900

    JS, Programmers, 해시 - 완주하지 못한 선수

--- Javascript/Programmers/Lv.0-1/129.완주하지_못한_선수.js
+++ Javascript/Programmers/Lv.0-1/129.완주하지_못한_선수.js
@@ -1,8 +1,19 @@
+// 1.
 const solution = (participant, completion) => {
   const arr1 = participant.sort();
   const arr2 = completion.sort();
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return arr1[i];
+  }
+};
+
+// 2.
+const solution2 = (participant, completion) => {
+  const p = participant.sort();
+  const c = completion.sort();
+
+  for (let i = 0; i < p.length; i++) {
+    if (p[i] !== c[i]) return p[i];
   }
 };
