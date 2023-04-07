4048bb3945898b2d5f0f2a0fe533a3876416a152
Owall <97934878+MayOwall@users.noreply.github.com>
2023-04-07 13:11:00 +0900

    JS, Programmers, 해시 - 위장

--- Javascript/Programmers/Lv.2/19. 위장.js
+++ Javascript/Programmers/Lv.2/19. 위장.js
@@ -1,4 +1,22 @@
-const solution = (clothes) => {
+// 1
+function solution1(clothes) {
+  let answer = 1;
+  const clothInfo = {};
+
+  clothes.forEach((a) => {
+    const [name, type] = a;
+    clothInfo[type] ? clothInfo[type].push(name) : (clothInfo[type] = [name]);
+  });
+
+  for (const key in clothInfo) {
+    answer *= clothInfo[key].length + 1;
+  }
+
+  return answer - 1;
+}
+
+// 2
+const solution2 = (clothes) => {
   const closet = {};
   clothes.forEach(([a, b]) =>
     closet[b] ? closet[b].push(a) : (closet[b] = [a])
@@ -12,22 +30,18 @@ const solution = (clothes) => {
   return answer;
 };
 
-// 예전 풀이
-/*
-function solution(clothes) {
-    let answer = 1;
-    const clothInfo = {};
-    
-    clothes.forEach(a => {
-        const [name, type] = a;
-        clothInfo[type] ? clothInfo[type].push(name) : clothInfo[type] = [name];
-    });
-    
-    
-    for(const key in clothInfo) {
-        answer *= clothInfo[key].length + 1;
-    };
-    
-    return answer - 1;
-}
-*/
+// 3
+const solution3 = (clothes) => {
+  let answer = 1;
+  const closet = {};
+
+  clothes.forEach(([cloth, type]) =>
+    closet[type] ? closet[type].push(cloth) : (closet[type] = [cloth])
+  );
+
+  for (let value of Object.values(closet)) {
+    answer *= value.length + 1;
+  }
+
+  return answer - 1;
+};
