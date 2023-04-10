4b0ec74e6b5b090f164b746327b54e504e025efc
Owall <97934878+MayOwall@users.noreply.github.com>
2023-04-10 13:29:04 +0900

    JS, Programmers, 기능개발

--- Javascript/Programmers/Lv.2/20. 기능개발.js
+++ Javascript/Programmers/Lv.2/20. 기능개발.js
@@ -1,3 +1,4 @@
+// 1.
 const solution = (progresses, speeds) => {
   const answer = [];
   const arr = progresses.map((v, idx) => Math.ceil((100 - v) / speeds[idx]));
@@ -20,5 +21,31 @@ const solution = (progresses, speeds) => {
   }
 
   answer.push(temp.length);
+  return answer;
+};
+
+// 2.
+const solution2 = (progresses, speeds) => {
+  const days = progresses.map((v, i) => {
+    const left = 100 - v;
+    const leftDay = Math.ceil(left / speeds[i]);
+    return leftDay;
+  });
+
+  const answer = [];
+  let acc = 1;
+  let curMax = days[0];
+
+  for (let i = 1; i < days.length; i++) {
+    if (days[i] > curMax) {
+      answer.push(acc);
+      acc = 1;
+      curMax = days[i];
+    } else {
+      acc += 1;
+    }
+  }
+
+  answer.push(acc);
   return answer;
 };
