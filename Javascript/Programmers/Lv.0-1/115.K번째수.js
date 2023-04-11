b16c7967ecdbbce3befc0daa86b810185dc2b2e3
Owall <97934878+MayOwall@users.noreply.github.com>
2023-04-11 12:24:34 +0900

    JS, Programmers, 정렬 - K번째수

--- Javascript/Programmers/Lv.0-1/115.K번째수.js
+++ Javascript/Programmers/Lv.0-1/115.K번째수.js
@@ -1,5 +1,13 @@
+// 1.
 const solution = (array, commands) =>
   commands.map(
     ([start, end, num]) =>
       array.slice(start - 1, end).sort((a, b) => a - b)[num - 1]
+  );
+
+// 2.
+const solution2 = (array, commands) =>
+  commands.map(
+    ([start, end, i]) =>
+      array.slice(start - 1, end).sort((a, b) => a - b)[i - 1]
   );
