7d4f9f638834fb9eaff24dd73eb238d8bda11787
Owall <97934878+MayOwall@users.noreply.github.com>
2023-04-11 12:32:32 +0900

    JS, Programers, 정렬 - 가장 큰 수

--- Javascript/Programmers/Lv.2/33. 가장 큰 수.js
+++ Javascript/Programmers/Lv.2/33. 가장 큰 수.js
@@ -1,15 +1,24 @@
-const makeStr = (v) => String(v) + String(v) + String(v);
+// const makeStr = (v) => String(v) + String(v) + String(v);
+
+// const solution = (numbers) => {
+//   if (numbers.every((v) => !v)) return "0";
+
+//   const arr = numbers.sort((a, b) => {
+//     const str1 = makeStr(a);
+//     const str2 = makeStr(b);
+//     const length = Math.min(str1.length, str2.length);
+
+//     return Number(str2.slice(0, length)) - Number(str1.slice(0, length));
+//   });
+
+//   return arr.join("");
+// };
 
 const solution = (numbers) => {
-  if (numbers.every((v) => !v)) return "0";
-
-  const arr = numbers.sort((a, b) => {
-    const str1 = makeStr(a);
-    const str2 = makeStr(b);
-    const length = Math.min(str1.length, str2.length);
-
-    return Number(str2.slice(0, length)) - Number(str1.slice(0, length));
-  });
-
+  const arr = numbers.sort(
+    (a, b) =>
+      Number(b.toString() + a.toString()) - Number(a.toString() + b.toString())
+  );
+  if (![...new Set(arr)][0]) return "0";
   return arr.join("");
 };
