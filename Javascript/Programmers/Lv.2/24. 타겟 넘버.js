5563703649efc6513e2c87ea7d250f3ac6a26b62
Owall <97934878+MayOwall@users.noreply.github.com>
2023-06-22 10:49:40 +0900

    JS, Programmers, 타겟 넘버

--- Javascript/Programmers/Lv.2/24. 타겟 넘버.js
+++ Javascript/Programmers/Lv.2/24. 타겟 넘버.js
@@ -1,15 +1,15 @@
 const solution = (numbers, target) => {
   let answer = 0;
-  const num = Math.pow(2, numbers.length) - 1;
 
-  for (let i = 0; i <= num; i++) {
-    const str = i.toString(2).padStart(numbers.length, "0");
-    const sum = numbers.reduce(
-      (acc, cur, idx) => (str[idx] === "0" ? acc - cur : acc + cur),
-      0
-    );
-    if (sum === target) answer += 1;
-  }
+  const dfs = (arr, sum) => {
+    if (!arr.length) {
+      if (sum === target) answer += 1;
+      return;
+    }
+    dfs(arr.slice(1), sum + arr[0]);
+    dfs(arr.slice(1), sum - arr[0]);
+  };
 
+  dfs(numbers, 0);
   return answer;
 };
