6e31880a9488eacccb620d2dafdc7d4cb115a433
Owall <97934878+MayOwall@users.noreply.github.com>
2023-06-22 21:26:52 +0900

    JS, Kakao, [3차] 파일명 정렬

--- Javascript/Kakao/[3차] 파일명 정렬.js
+++ Javascript/Kakao/[3차] 파일명 정렬.js
@@ -1,14 +1,19 @@
-function solution(files) {
+const splitFile = (file) => {
+  const head = file.split(/[0-9]{1,}/)[0].toLowerCase();
+  const num = file.split(/[^0-9]{1,}/)[1] * 1;
+  const rest = file.replace(new RegExp(`${head}${num}`), "");
+  return [head, num, rest];
+};
+
+const solution = (files) => {
   return files.sort((a, b) => {
-    const aHead = a.match(/^\D+/)[0].toLowerCase();
-    const bHead = b.match(/^\D+/)[0].toLowerCase();
+    const A = splitFile(a);
+    const B = splitFile(b);
 
-    if (aHead < bHead) return -1;
-    if (aHead > bHead) return 1;
-
-    const aNum = a.match(/\d+/)[0].replace(/^0+/, "");
-    const bNum = b.match(/\d+/)[0].replace(/^0+/, "");
-
-    return aNum - bNum;
+    if (A[0] < B[0]) return -1;
+    if (A[0] > B[0]) return 1;
+    if (A[1] < B[1]) return -1;
+    if (A[1] > B[1]) return 1;
+    return 0;
   });
-}
+};
