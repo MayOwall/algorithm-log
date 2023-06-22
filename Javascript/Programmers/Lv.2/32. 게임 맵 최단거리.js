dd974d93341313036c2f328873a2893c71b4a629
Owall <97934878+MayOwall@users.noreply.github.com>
2023-06-22 18:14:54 +0900

    JS, Programmers, 게임 맵 최단거리

--- Javascript/Programmers/Lv.2/32. 게임 맵 최단거리.js
+++ Javascript/Programmers/Lv.2/32. 게임 맵 최단거리.js
@@ -1,34 +1,29 @@
-function solution(maps) {
-  const N = maps.length - 1;
-  const M = maps[0].length - 1;
-  const direction = [
-    [0, -1],
-    [0, 1],
-    [-1, 0],
-    [1, 0],
-  ];
+const arrows = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
 
-  let queue = [[0, 0, 1]];
+const solution = (maps) => {
+  const C = maps.length - 1;
+  const R = maps[0].length - 1;
   maps[0][0] = 0;
+  const queue = [[0, 0, 1]];
 
-  while (queue.length > 0) {
-    const [x, y, count] = queue.shift();
-    if (x == N && y == M) {
-      return count;
-    }
+  while (!!queue.length) {
+    const [c, r, count] = queue.shift();
+    if (c === C && r === R) return count;
 
-    for (d of direction) {
-      const next = [x + d[0], y + d[1], count + 1];
-      if (
-        maps[next[0]] &&
-        maps[next[0]][next[1]] &&
-        maps[next[0]][next[1]] === 1
-      ) {
+    maps[c][r] = 0;
+    for (let arrow of arrows) {
+      const next = [c + arrow[0], r + arrow[1], count + 1];
+      if (!!maps[next[0]] && maps[next[0]][next[1]] === 1) {
         maps[next[0]][next[1]] = 0;
-
         queue.push(next);
       }
     }
   }
+
   return -1;
-}
+};
