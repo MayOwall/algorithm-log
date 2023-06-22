b46c47f48ad7949685e11db1d6c74ceced6b544f
Owall <97934878+MayOwall@users.noreply.github.com>
2023-06-22 15:29:48 +0900

    JS, Programmers, 땅따먹기

--- Javascript/Programmers/Lv.2/28. 땅따먹기.js
+++ Javascript/Programmers/Lv.2/28. 땅따먹기.js
@@ -1,13 +1,8 @@
-const solution = (land) => {
-  for (let i = 1; i < land.length; i++) {
-    const newArr = land[i].map((v, idx) => {
-      const lastArr = land[i - 1];
-      const filtered = [...lastArr.slice(0, idx), ...lastArr.slice(idx + 1)];
-      const max = Math.max(...filtered);
-      return v + max;
-    });
-    land[i] = newArr;
-  }
-
-  return Math.max(...land[land.length - 1]);
-};
+const solution = (land) =>
+  Math.max(
+    ...land.reduce(
+      (a, c) =>
+        c.map((v, i) => v + Math.max(...a.filter((_, aIdx) => i !== aIdx))),
+      [0, 0, 0, 0]
+    )
+  );
