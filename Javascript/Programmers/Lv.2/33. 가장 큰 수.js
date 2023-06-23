69f9df60809d652e3c3b60653363acd3e7bc7f27
Owall <97934878+MayOwall@users.noreply.github.com>
2023-06-23 15:25:52 +0900

    JS, Programmers, 가장 큰 수

--- Javascript/Programmers/Lv.2/33. 가장 큰 수.js
+++ Javascript/Programmers/Lv.2/33. 가장 큰 수.js
@@ -1,24 +1,11 @@
-// const makeStr = (v) => String(v) + String(v) + String(v);
-
-// const solution = (numbers) => {
-//   if (numbers.every((v) => !v)) return "0";
-
-//   const arr = numbers.sort((a, b) => {
-//     const str1 = makeStr(a);
-//     const str2 = makeStr(b);
-//     const length = Math.min(str1.length, str2.length);
-
-//     return Number(str2.slice(0, length)) - Number(str1.slice(0, length));
-//   });
-
-//   return arr.join("");
-// };
-
 const solution = (numbers) => {
-  const arr = numbers.sort(
-    (a, b) =>
-      Number(b.toString() + a.toString()) - Number(a.toString() + b.toString())
-  );
-  if (![...new Set(arr)][0]) return "0";
-  return arr.join("");
+  if (!(numbers.join("") * 1)) return "0";
+
+  const sort = numbers.sort((a, b) => {
+    const caseA = (a.toString() + b.toString()) * 1;
+    const caseB = (b.toString() + a.toString()) * 1;
+    return caseA < caseB ? 1 : caseA > caseB ? -1 : 0;
+  });
+
+  return sort.join("");
 };
