12daf0768029b53c1991aa0a7f988ce0a8d3e264
Owall <97934878+MayOwall@users.noreply.github.com>
2023-06-23 15:54:35 +0900

    JS, Programmers, 소수 찾기

--- Javascript/Programmers/Lv.2/43. 소수 찾기.js
+++ Javascript/Programmers/Lv.2/43. 소수 찾기.js
@@ -1,42 +1,27 @@
-const solution = (numbers) => {
-  const arr = numbers.split("");
-  let nums = [];
-
-  // 조합 가능한 숫자를 찾는 로직
-  const permutation = (output, rests, length) => {
-    if (output.length === length) {
-      if (!nums.includes(output * 1)) nums.push(output * 1);
-      return;
-    }
-    rests.forEach((v, i) => {
-      const nextOutput = `${output}${v}`;
-      const nextRests = rests.filter((_, restIdx) => restIdx !== i);
-      permutation(nextOutput, nextRests, length);
-    });
-  };
-
-  // arr의 모든 값들을 시작값으로 지정하기 위해 반복문 수행
-  for (let i = 0; i < arr.length; i++) {
-    const firstSelect = arr[i];
-    const firstRests = arr.filter((_, restIdx) => restIdx !== i);
-
-    // 1부터 최대 자릿수까지 순회
-    for (let j = 1; j <= arr.length; j++) {
-      permutation(firstSelect, firstRests, j);
-    }
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (!(num % i)) return false;
   }
+  return true;
+};
 
-  // 소수 판별 핸들러
-  const checkPrimeNumber = (number) => {
-    if (number < 2) return false;
+const solution = (numbers) => {
+  let answer = [];
+  numbers = numbers.split("");
+  const isVisit = Array.from({ length: numbers.length }, () => false);
 
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-      const remainder = number % i;
-      if (remainder === 0) return false;
-    }
-    return true;
+  const dfs = (i, sum) => {
+    if (isPrime(sum * 1) && !answer.includes(sum * 1)) answer.push(sum * 1);
+    if (sum.length === numbers.length) return;
+
+    isVisit[i] = true;
+    numbers.forEach((num, i) => {
+      if (!isVisit[i]) dfs(i, sum + num);
+    });
+    isVisit[i] = false;
   };
 
-  nums = nums.filter((v) => checkPrimeNumber(v * 1));
-  return nums.length;
+  numbers.forEach((num, i) => dfs(i, num));
+  return answer.length;
 };
