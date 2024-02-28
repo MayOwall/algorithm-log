6b8ccfbabfb5e0d5b41d0c6367f061fa541ac9a0
이지은 (오월) <pixartive@gmail.com>
2024-02-28 14:36:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 102.78 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,8 +1,14 @@
 const solution = (participant, completion) => {
-    const sortP = participant.sort();
-    const sortC = completion.sort();
+    // paticipant와 completion을 알파벳에 따라 정렬한다.
+    participant = participant.sort();
+    completion = completion.sort();
     
-    for(let i = 0; i < sortP.length; i++) {
-        if(sortP[i] !== sortC[i]) return sortP[i];
+    // participant 순회하며 같은 인덱스의 completion과 다른 값이 있는지 확인한다.
+    for(let i = 0; i < participant.length; i++) {
+        const curParticipant = participant[i];
+        const curCompletion = completion[i];
+        
+        // 다른 값이 있다면 해당 participant 값을 리턴한다.
+        if(curParticipant !== curCompletion) return curParticipant;
     }
 }
