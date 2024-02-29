94ea5fb22670e617c093b9d3e2dd9b779afa92ee
MayOwall <pixartive@gmail.com>
2024-02-29 12:17:33 +0900

    heap 보완

--- Javascript/자료구조/heap-priorityQueue.js
+++ Javascript/자료구조/heap-priorityQueue.js
@@ -5,9 +5,10 @@ class MinHeap {
   }
 
   isExist = () => this.heap.length > this.FIRST_INDEX;
+  getMin = () => this.heap[this.FIRST_INDEX];
   getParentIndex = (n) => Math.floor(n / 2);
   getLeftChildIndex = (n) => n * 2;
-  getRightchildIndex = (n) => n * 2 + 1;
+  getRightChildIndex = (n) => n * 2 + 1;
 
   insert = (value) => {
     this.heap.push(value);
@@ -21,52 +22,48 @@ class MinHeap {
     }
 
     const root = this.heap[this.FIRST_INDEX];
-
     this.heap[this.FIRST_INDEX] = this.heap.pop();
     this.heapifyDown();
-
     return root;
   };
 
   heapifyUp = () => {
     let index = this.heap.length - 1;
-    const lastInsertNode = this.heap[index];
+    const lastInsertedNode = this.heap[index];
 
     while (index > this.FIRST_INDEX) {
+      const curNode = this.heap[index];
       const parentIndex = this.getParentIndex(index);
       const parentNode = this.heap[parentIndex];
-      const curNode = this.heap[index];
-      if (parentNode > curNode) {
+
+      if (parentNode > lastInsertedNode) {
         this.heap[index] = parentNode;
         index = parentIndex;
       } else break;
     }
 
-    this.heap[index] = lastInsertNode;
+    this.heap[index] = lastInsertedNode;
   };
-
   heapifyDown = () => {
     let index = this.FIRST_INDEX;
-    const rootNode = this.heap[index];
+    const root = this.heap[index];
 
-    while (index < this.heap.length - 1) {
+    while (this.getLeftChildIndex(index) < this.heap.length) {
       const curNode = this.heap[index];
       const leftChildIndex = this.getLeftChildIndex(index);
       const leftChild = this.heap[leftChildIndex];
-      const rightChildIndex = this.getRightchildIndex(index);
+      const rightChildIndex = this.getRightChildIndex(index);
       const rightChild = this.heap[rightChildIndex];
-
       const smallerChildIndex =
         rightChild && leftChild > rightChild ? rightChildIndex : leftChildIndex;
       const smallerChild = this.heap[smallerChildIndex];
-
-      if (smallerChild < curNode) {
+      if (smallerChild < root) {
         this.heap[index] = smallerChild;
         index = smallerChildIndex;
       } else break;
     }
 
-    this.heap[index] = rootNode;
+    this.heap[index] = root;
   };
 }
 
