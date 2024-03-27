39ffff0eae7b8922efd08ad6e473d5789ff4649c
이지은 (오월) <pixartive@gmail.com>
2024-03-27 13:36:11 +0900

    [level 3] Title: 베스트앨범, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,32 +1,18 @@
-const solution = (genres, plays) => {
-    const streamingInfo = {};
+function solution(genres, plays) {
+    // 알아야 하는 것 : 각 장르별 총 재생횟수, 각 장르의 노래별 재생 횟수
     
-    // 노래들을 순회하며 장르 총 재생 횟수와 장르별 노래에 대한 정보를 얻는다.
-    genres.forEach((genre, id) => {
-        const play = plays[id];
-        const song = [id, play];
-        
-        if(!streamingInfo[genre]) {
-            streamingInfo[genre] = {
-                playSum : 0,
-                songs : []
-            }
-        };
-        
-        streamingInfo[genre].playSum += play;
-        streamingInfo[genre].songs.push(song);
+    const stats = {};
+    genres.forEach((genre, i) => {
+        if(!stats[genre]) stats[genre] = {sum : 0, songs : []};
+        const song = plays[i];
+        stats[genre].sum += song;
+        stats[genre].songs.push([i, song]);
     })
     
-    // 가장 많이 재생된 장르 2개를 선정한다.
-    const mostPlayedGenres = Object.keys(streamingInfo).sort((a, b) => streamingInfo[b].playSum - streamingInfo[a].playSum);
-
-    // 각 장르별 가장 많이 재생된, 그리고 고유번호가 낮은 노래 2개를 선정한다.
-    const mostPlayedSongIds = mostPlayedGenres.map(genre => getMostPlayedSongIds(streamingInfo, genre));
-    return mostPlayedSongIds.flat();
-}
-
-const getMostPlayedSongIds = (streamingInfo, genre) => {
-    const songs = streamingInfo[genre].songs;
-    const mostPlayedSongs = songs.sort((a, b) => b[1] - a[1]).slice(0, 2);
-    return mostPlayedSongs.map(song => song[0]);
+    const getBestTwoSongs = (songs) => {
+        return songs.sort((a, b) => b[1] - a[1]).slice(0, 2);
+    };
+    
+    const bestGenres = Object.keys(stats).sort((a, b) => stats[b].sum - stats[a].sum);
+    return bestGenres.map(v => getBestTwoSongs(stats[v].songs).map(v => v[0])).flat();
 }
