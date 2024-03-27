19da3410b9f3ceb71851e4af657f7bcba6d657eb
이지은 (오월) <pixartive@gmail.com>
2024-03-27 13:28:57 +0900

    [level 2] Title: 의상, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,14 +1,9 @@
-const solution = (clothes) => {
+function solution(clothes) {
     const closet = {};
-    
-    // 옷들을 종류별로 분류
-    clothes.forEach(cloth => {
-        const [name, type] = cloth;
-        if(!closet[type]) closet[type] = 0;
-        closet[type] += 1;
+    clothes.forEach(([cloth, type]) => {
+        if(!closet[type]) closet[type] = {};
+        if(!closet[type][cloth]) closet[type][cloth] = true;
     });
-    
-    // 경우의 수를 조합
-    const result = Object.values(closet).reduce((acc, cur) => acc * (cur + 1), 1) - 1;
-    return result;
+
+    return Object.values(closet).reduce((acc, cur) => acc * (Object.keys(cur).length + 1), 1) - 1;
 }
