5f5b2702d5c64fb18dfba0ee41f1a57c049da06f
이지은 (오월) <pixartive@gmail.com>
2024-03-27 13:03:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 129.12 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 58.1 MB, 시간: 102.78 ms
+메모리: 58.8 MB, 시간: 129.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 28일 14:36:00
+2024년 03월 27일 13:03:54
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,14 +1,10 @@
-const solution = (participant, completion) => {
-    // paticipant와 completion을 알파벳에 따라 정렬한다.
-    participant = participant.sort();
-    completion = completion.sort();
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
     
-    // participant 순회하며 같은 인덱스의 completion과 다른 값이 있는지 확인한다.
     for(let i = 0; i < participant.length; i++) {
-        const curParticipant = participant[i];
-        const curCompletion = completion[i];
-        
-        // 다른 값이 있다면 해당 participant 값을 리턴한다.
-        if(curParticipant !== curCompletion) return curParticipant;
-    }
+        const a = participant[i];
+        const b = completion[i];
+        if(a !== b) return a;
+    };
 }
