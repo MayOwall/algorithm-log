91460e49f4fe05945ab2299232b29790c88f901d
이지은 (오월) <pixartive@gmail.com>
2024-03-27 19:07:45 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,22 +1,23 @@
-// 1. 각 작업별로 배포되는데 소요되는 날짜 배열을 구한다.
-// 2. 1의 배열을 순회하며 현재 인덱스보다 같거나 작은 값의 값이 얼마나 연속되는지 구한다.
-// 3. 구한 값들을 배열로 리턴한다.
-const solution = (pregresses, speeds) => {
-    const days = pregresses.map((progress, i) => Math.ceil((100 - progress) / speeds[i]));
+function solution(progresses, speeds) {
+    progresses = progresses.map((v, i) => {
+        return Math.ceil((100 - v) / speeds[i]);
+    });
+    
     const answer = [];
+    let count = 1;
+    let max = progresses[0];
     
-    const sum = days.reduce((acc, cur) => {
-        if(acc.max < cur) {
-            if(acc.count) answer.push(acc.count);
-            acc.max = cur;
-            acc.count = 1;
-            return acc;
+    for(let i = 1; i < progresses.length; i++) {
+        const progress = progresses[i];
+        if(progress > max) {
+            answer.push(count);
+            max = progress;
+            count = 1;
+            continue;
         }
-        
-        acc.count += 1;
-        return acc;
-    }, {max : 0, count : 0});
+        count += 1;
+    };
     
-    if(sum.count) answer.push(sum.count);
+    if(count) answer.push(count);
     return answer;
 }
