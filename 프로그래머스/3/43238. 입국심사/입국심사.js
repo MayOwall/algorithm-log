724a075a82fa9a5a6cf55be1b9429d01d3e183ae
이지은 (오월) <pixartive@gmail.com>
2024-03-28 10:28:53 +0900

    [level 3] Title: 입국심사, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,22 +1,22 @@
-const solution = (n, times) => {
-  let left = 1;
-  let right = n * Math.max(...times);
-  let answer = right;
-
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    const capacivity = getCapacivity(times, mid);
-    if (capacivity >= n) {
-      right = mid - 1;
-      answer = answer > mid ? mid : answer;
-    } else {
-      left = mid + 1;
+function solution (n, times) {
+    let left = 0;
+    let right = Math.pow(2, 52);
+    let count = 0;
+    
+    while(count++ < 54 && left <= right) {
+        const mid = Math.ceil((left + right) / 2);
+        if(isAble(mid)) {
+            right = mid - 1;
+            continue;
+        } else {
+            left = mid + 1;
+        }
     }
-  }
-
-  return answer;
-};
-
-const getCapacivity = (times, mid) => {
-  return times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
-};
+    
+    return left;
+    
+    function isAble (time) {
+        const capacity = times.map(v => Math.floor(time / v)).reduce((acc, cur)=> acc + cur);
+        return n <= capacity;
+    }
+}
