51d681d06dc4146d831f8d44cb4460334319ca0f
MayOwall <pixartive@gmail.com>
2024-04-16 20:08:45 +0900

    fix mergeSort-recursion

--- 자료구조/분할정복/mergeSort-recursion.js
+++ 자료구조/분할정복/mergeSort-recursion.js
@@ -12,12 +12,11 @@
  * - 조건이 완료된 후, 기존 두 배열의 남은 원소를 결과 배열에 넣어준다.
  * - 결과 배열을 리턴한다.
  */
-
 class MergeSort {
-  static desc = (arr) => this.#sort(arr, this.#mergeDesc);
-  static asce = (arr) => this.#sort(arr, this.#mergeAsce);
+  static desc = (arr) => this.#sort(arr, "desc");
+  static asce = (arr) => this.#sort(arr, "asce");
 
-  static #sort(arr, mergeUtil) {
+  static #sort(arr, type) {
     if (!arr.length || arr.length === 1) {
       return arr;
     }
@@ -26,12 +25,12 @@ class MergeSort {
     const arr1 = arr.slice(0, mid);
     const arr2 = arr.slice(mid);
 
-    const sortedArr1 = this.#sort(arr1, mergeUtil);
-    const sortedArr2 = this.#sort(arr2, mergeUtil);
+    const sortedArr1 = this.#sort(arr1, type);
+    const sortedArr2 = this.#sort(arr2, type);
 
-    return mergeUtil(sortedArr1, sortedArr2);
+    return this.#merge(sortedArr1, sortedArr2, type);
   }
-  static #mergeDesc(arr1, arr2) {
+  static #merge = (arr1, arr2, type) => {
     const result = [];
     let i = 0;
     let j = 0;
@@ -39,63 +38,33 @@ class MergeSort {
     while (i < arr1.length && j < arr2.length) {
       const cur1 = arr1[i];
       const cur2 = arr2[j];
-
       if (cur1 === cur2) {
         result.push(cur1, cur2);
         i += 1;
         j += 1;
       }
-      if (cur1 > cur2) {
-        result.push(cur1);
-        i += 1;
-      }
-      if (cur1 < cur2) {
+      if (
+        (type === "asce" && cur1 > cur2) ||
+        (type === "desc" && cur1 < cur2)
+      ) {
         result.push(cur2);
         j += 1;
+        continue;
       }
+
+      result.push(cur1);
+      i += 1;
     }
 
-    result.push(...arr1.slice(i));
-    result.push(...arr2.slice(j));
-
-    return result;
-  }
-  static #mergeAsce(arr1, arr2) {
-    const result = [];
-    let i = 0;
-    let j = 0;
-
-    while (i < arr1.length && j < arr2.length) {
-      const cur1 = arr1[i];
-      const cur2 = arr2[j];
-
-      if (cur1 === cur2) {
-        result.push(cur1, cur2);
-        i += 1;
-        j += 1;
-      }
-      if (cur1 < cur2) {
-        result.push(cur1);
-        i += 1;
-      }
-      if (cur1 > cur2) {
-        result.push(cur2);
-        j += 1;
-      }
-    }
-
-    result.push(...arr1.slice(i));
-    result.push(...arr2.slice(j));
-
-    return result;
-  }
+    return [...result, ...arr1.slice(i), ...arr2.slice(j)];
+  };
 }
 
-// 임의의 20개의 배열 테스트
-const arr = Array.from({ length: 20 }, () => Math.floor(Math.random() * 100));
-console.log("origin", arr.length);
-console.log("desc", MergeSort.desc(arr).length);
-console.log("asce", MergeSort.asce(arr).length);
+// // 임의의 20개의 배열 테스트
+// const arr = Array.from({ length: 20 }, () => Math.floor(Math.random() * 100));
+// console.log("origin", arr);
+// console.log("desc", MergeSort.desc(arr));
+// console.log("asce", MergeSort.asce(arr));
 
-// 빈 배열 테스트
-console.log("empty", MergeSort.desc([]));
+// // 빈 배열 테스트
+// console.log("empty", MergeSort.desc([]));
