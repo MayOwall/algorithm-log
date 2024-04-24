ea32753f167d918431980ce18cb4e76a5de3417f
이지은 (오월) <pixartive@gmail.com>
2024-04-24 15:25:21 +0900

    [Bronze II] Title: 수 정렬하기, Time: 132 ms, Memory: 9760 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,6 +1,5 @@
-// Node 내장 정렬
+let [_, ...inputs] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [_, ...inputs] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-
+inputs = inputs.map(v => v * 1);
 const answer = inputs.sort((a, b) => Number(a) - Number(b)).join('\n');
 console.log(answer);
